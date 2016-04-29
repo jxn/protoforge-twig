43a52442f85a96f1d4d1daad595aaa6395c995ca
Jackson Murtha <jackson@blendinteractive.com>
2016-04-29 01:24:55 -0500

    start.sh now inits a project, destroys old git repository, and starts PHP server

---
+++ start.sh
@@ -0,0 +1,47 @@
+#! /bin/bash
+
+BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+BASE_DIR_NAME="$(basename $BASE_DIR)"
+HTDOCS="$BASE_DIR"'/htdocs'
+PORT='8080'
+
+# Set a custom port
+if [[ ! -z "$1" ]]; then
+    PORT="$1"
+fi
+
+cd "$BASE_DIR"
+if grep -q 'jxn/protoforge-twig.git' '.git/config' ; then
+    echo "Removing temporary git repo..."
+#    rm -Rf '.git/'
+    echo 'done.'
+    echo "Initializing new Git repo..."
+#    git init
+fi
+
+cd "$BASE_DIR"
+if [ -f "composer.json" ] ; then
+
+    echo "Running composer install"
+    cd "$BASE_DIR"
+
+    global_composer=$(command -v composer 2>/dev/null)
+    if [ "$global_composer" != "" ] ; then
+        echo "Using global composer"
+        composer install
+    else
+        if [ ! -f "composer.phar" ] ; then
+            wget 'https://getcomposer.org/composer.phar'
+        fi
+        php -d detect_unicode=Off composer.phar install
+    fi
+fi
+
+echo "Opening htdocs directory"
+cd "$BASE_DIR""/htdocs"
+
+echo "Running PHP server on port ""$PORT"
+echo "Visit http://localhost:""$PORT""/"
+echo "To specify a port, pass it as an argument to the start.sh script"
+echo "e.g. ""$0"" ""8082"
+php -S "localhost:""$PORT"
